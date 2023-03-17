0d5dfd4eae04469629d3ee90e014ec07420cab27
Vladislav Fedotov <vladislav.fedotov@simbirsoft.com>
2023-03-17 13:52:33 +0400

    added directory crc32 sum script

---
+++ crc32-sum-folder.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Expose md5 hashsum of files, passed as an argument.
+#
+# Usage: add this to crontab:
+#
+# */5 * * * * prometheus md5-hashsum.sh /var/lib/prometheus | sponge /var/lib/node_exporter/md5-hashsum.prom
+#
+# 
+echo "# HELP crc32 sum CRC32 sum of folder"
+echo "# TYPE crc32_sum_folder gauge"
+find "$@"  -type f -exec cksum {} \; | sort -k 2 | cksum |  sed 's/ //'
