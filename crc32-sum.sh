d435d662d0b684090a172a9cbffde9c41d5e8ad7
Vladislav Fedotov <vladislav.fedotov@simbirsoft.com>
2023-03-17 10:47:31 +0400

    edit hash algorithm

--- crc32-sum.sh
+++ crc32-sum.sh
@@ -7,6 +7,6 @@
 # */5 * * * * prometheus md5-hashsum.sh /var/lib/prometheus | sponge /var/lib/node_exporter/md5-hashsum.prom
 #
 # 
-echo "# HELP md5_hashsum MD5 hashsum of file"
-echo "# TYPE md5_hashsum gauge"
-md5sum "$@" | sed 's/\(.*\) \(.*\)/md5_hashsum{file="\2"} \1/'
+echo "# HELP crc32 sum CRC32 sum of file"
+echo "# TYPE crc32_sum gauge"
+cksum "$@" | sed 's/\(.*\) \(.*\)/crc32_sum{file="\2"} \1/'
